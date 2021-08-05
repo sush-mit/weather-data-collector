2ae962b4a0c790d4709b2d0b54c0621697ad0dfb
sush-mit <sushmitpradhan33@gmail.com>
2021-08-05 10:51:20 +0545

    implemented user registration

--- main/models.py
+++ main/models.py
@@ -1,6 +1,1 @@
 from django.db import models
-
-class UserAccountsModel(models.Model):
-    user_name = models.CharField(max_length=32)
-    user_email = models.CharField(max_length=256)
-    user_password = models.CharField(max_length=32)
