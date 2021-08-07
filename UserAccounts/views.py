4d7e76a3fbffb00a060e95929d499bffdfcb35c6
sush-mit <sushmitpradhan33@gmail.com>
2021-08-07 21:00:57 +0545

    added first and last name fields

--- UserAccounts/views.py
+++ UserAccounts/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -10,3 +11,6 @@ def userpage(request, username):
         return render(request, "UserAccounts/userpage.html")
     else:
         return redirect("/login/")
+=======
+from django.shortcuts import render
+>>>>>>> e1ad4587b88ceb53d74b62c0a2e5e8f967cf3a11
