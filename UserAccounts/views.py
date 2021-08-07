6d5fb6d20cfe6440eec680883859d8ec1cbe5238
sush-mit <sushmitpradhan33@gmail.com>
2021-08-07 21:06:25 +0545

    merge conflicts fixed

--- UserAccounts/views.py
+++ UserAccounts/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -11,6 +10,3 @@ def userpage(request, username):
         return render(request, "UserAccounts/userpage.html")
     else:
         return redirect("/login/")
-=======
-from django.shortcuts import render
->>>>>>> e1ad4587b88ceb53d74b62c0a2e5e8f967cf3a11
