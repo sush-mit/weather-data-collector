4961621d328c1f011bb86bcdf522b6f22e348e7a
sush-mit <sushmitpradhan33@gmail.com>
2021-08-11 21:23:05 +0545

    created Station table and implemented add/delete function for table record

--- Station/urls.py
+++ Station/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("station/input", views.station_input, name="station_input"),
     path("station/", views.station_data, name="station_data"),
+    path("station/delete/<int:id>", views.station_delete, name="station_delete"),
 ]
