3f7a5ce190fb54ed0f79adccc14814898304ab75
sush-mit <sushmitpradhan33@gmail.com>
2021-08-11 21:23:00 +0545

    created Station table and implemented add/delete function for table record

--- Station/views.py
+++ Station/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import StationInputForm
+from .models import StationModel
+
+def station_data(request):
+    data = StationModel.objects.all()
+    return render(request, "Station/data.html", {"data": data})
 
 def station_input(request):
+    if request.method == "POST":
+        form = StationInputForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect(f"/station")
+    else:
+        form = StationInputForm()
     form = StationInputForm()
     return render(request, "Station/input.html", {"form": form})
