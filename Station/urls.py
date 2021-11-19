58560707d8c9fb489790edea213d905e94342bb6
sush-mit <sushmitpradhan33@gmail.com>
2021-11-19 16:50:38 +0900

    Added 'created' and 'updated' rows and edit option (not usable).

--- Station/urls.py
+++ Station/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("station/input", views.station, name="station_data"),
-    path("station/", views.station, name="station_input"),
-    path("station/delete/<int:id>", views.station, name="station_delete"),
+    path("<str:uname>/station/", views.station, name="station_data"),
+    path("<str:uname>/station/input", views.station, name="station_input"),
+    path("<str:uname>/station/delete/<int:id_>", views.station, name="station_delete"),
 ]
--- Station/views.py
+++ Station/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render, redirect
 from .forms import StationInputForm
 from .models import StationModel
 
-def station(request):
+def station(request, uname=None, id_=None):
     if request.user.is_authenticated:
       if 'input' in request.path:
-        station_input(request)
+        return station_input(request,uname)
       elif 'delete' in request.path:
-        station_delete(request)
+        return station_delete(request, uname, id_)
+      elif 'delete' in request.path:
+        return station_edit(request, uname, id_)
       else:
-        station_data(request)
+        return station_data(request)
     else:    
       return HttpResponseForbidden()
   
@@ -18,20 +20,25 @@ def station_data(request):
   data = StationModel.objects.all()
   return render(request, "Station/data.html", {"data": data})
 
-def station_input(request):
+def station_input(request, uname):
       if request.method == "POST":
           form = StationInputForm(request.POST)
           if form.is_valid():
               form.save()
 
-              return redirect("/station")
+              return redirect(f"/{uname}/station")
       else:
           form = StationInputForm()
       form = StationInputForm()
       return render(request, "Station/input.html", {"form": form})
 
-def station_delete(request, id):
-    data_to_delete = StationModel.objects.get(id=id)
+def station_edit(request, uname, id_):
+  data_to_edit = StationModel.objects.get(id=id_)
+  data = StationModel.objects.all()
+  return redirect(f"/{uname}/station")
+
+def station_delete(request, uname, id_):
+    data_to_delete = StationModel.objects.get(id=id_)
     data_to_delete.delete()
     data = StationModel.objects.all()
-    return redirect("/station")
+    return redirect(f"/{uname}/station")
