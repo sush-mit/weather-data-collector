0d1ca51cc7d95442a1a809d403054a90ad86863b
sush-mit <sushmitpradhan33@gmail.com>
2021-11-21 20:54:28 +0900

    Changed url pattern for deleting station.

--- Station/urls.py
+++ Station/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path("<str:uname>/stations/", views.station, name="station_data"),
     path("<str:uname>/stations/input", views.station, name="station_input"),
-    path("<str:uname>/stations/delete/<int:id_>", views.station, name="station_delete"),
+    path("<str:uname>/stations/<int:id_>/delete/", views.station, name="station_delete"),
+    path("<str:uname>/stations/<int:id_>/edit/", views.station, name="station_edit"),
 ]
