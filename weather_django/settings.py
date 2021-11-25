30481d8512b711415971fdaffce14f6abb1d34d1
sush-mit <sushmitpradhan33@gmail.com>
2021-11-26 01:41:08 +0900

    Changed database to mariadb, added requirements file.

--- weather_django/settings.py
+++ weather_django/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "weather_django.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "weather",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "USER": "django",
+        "PASSWORD": "django_pass",
     }
 }
 
